056b4a7ce88d727a117dd009d2291ad9fe88fffb
Alexandr <a.a.shutov@bk.ru>
2024-04-16 10:07:57 +0300

    First commit

---
+++ WebTesting/src/main/java/ru/sh/WebTesting/WebTestingApplication.java
@@ -0,0 +1,13 @@
+package ru.sh.WebTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTestingApplication.class, args);
+	}
+
+}
---
+++ WebTesting/src/test/java/ru/sh/WebTesting/WebTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sh.WebTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
